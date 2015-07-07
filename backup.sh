d0a9cd80236eee08c8bbced9367158ea0967e995
@ontheroad_jp <dev@ontheroad.jp>
2015-07-07 09:21:17 +0900

    backup at 2015-07-07 09:21:17

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-content_dir="/Users/hideaki/Vagrant/LAMP/www/SPA-01"
+#content_dir="/Users/hideaki/Vagrant/LAMP/www/SPA-01"
+content_dir="$HOME/Vagrant/LAMP/www/SPA-01"
 
 set -x
 cd "$content_dir"
